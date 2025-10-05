4dea55fbd8c1d3f08c4609ad9f0213b3438f3c37
yves appriou <yvesappriou1@gmail.com>
2025-10-05 23:55:02 +0200

    Week 4 exercices, tags but without colors changed

---
+++ Week-4/ternus.py
@@ -0,0 +1,70 @@
+from tkinter import Canvas
+from expyriment import design, control, stimuli
+from expyriment.misc.constants import K_SPACE
+
+#collaboration with Lou Mallet
+
+exp = design.Experiment(name="ternus")
+
+control.set_develop_mode()
+control.initialize(exp)
+
+def add_tags(circle,radius):
+    pos=(0,0)
+    #pos=(radius//2, -radius//2)
+    tag = stimuli.Circle(int(radius/3), colour=(255,0,0),position=pos)
+    tag.plot(circle)
+
+def make_circles(radius,tags):
+    dist = radius * 3 
+    circles = []
+    for i in range(2):
+        circle = stimuli.Circle(radius, colour=(255,255,255), position=((i - 1) * dist, 0))
+        if tags:
+            add_tags(circle, radius)
+        circle.preload()
+        circles.append(circle)
+    return circles
+
+def present_for(stims,time):
+    stims.present()
+    exp.clock.wait(time)
+    exp.screen.clear()
+
+def run_trial(radius, isi, tags):
+    # First frame: left 3 circles
+    circle1 = make_circles(radius, tags)
+    canv1 = stimuli.BlankScreen(colour=(0,0,0))
+    for c in circle1: 
+        c.plot(canv1)
+
+    # Second frame: right-shifted 3 circles
+    circle2 = make_circles(radius, tags)
+    shift = radius * 3
+    for c in circle2: 
+        c.move((shift,0))
+    canv2 = stimuli.BlankScreen(colour=(0,0,0))
+    for c in circle2: 
+        c.plot(canv2)
+
+    # Loop until SPACE is pressed
+    while True:
+        if isi > 0:
+            exp.screen.update()
+            exp.clock.wait(int(isi * 1000/60))
+        present_for(canv1, 200)   # 200 ms approx
+        if isi > 0:
+            exp.screen.update()
+            exp.clock.wait(int(isi * 1000/60))
+        present_for(canv2, 200)
+        
+        if exp.keyboard.check(K_SPACE):
+            break
+
+
+""" Test functions """
+
+run_trial(50, 0, False)
+
+
+control.end()
